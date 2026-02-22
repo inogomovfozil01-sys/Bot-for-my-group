47a477530091553072b665e776c0decd82381d8c
inogomovfozil01-sys <inogomovfozil01@gmail.com>
2026-02-22 15:45:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,12 @@
 module.exports = {
-    TOKEN: process.env.BOT_TOKEN || '8529939811:AAEE2XMUZ5RkMxqWHLL86OBSGW45-sIoQzQ',
+    TOKEN: process.env.BOT_TOKEN || '8550705709:AAGpKWiMEsterjXk1FBHOUETLfv6-KyQCro',
     OWNER_ID: process.env.OWNER_ID || '6690476979',
     TEACHER_ID: process.env.TEACHER_ID || '5092858512',
     GROUP_ID: process.env.GROUP_ID || '-1002582621647',
 
     // Chat where teacher support threads are created (forum supergroup recommended)
-    TEACHER_CHAT_ID: process.env.TEACHER_CHAT_ID || process.env.GROUP_ID || '-1003747817914',
+    TEACHER_CHAT_ID: process.env.TEACHER_CHAT_ID || process.env.GROUP_ID || '-1003546840118',
 
     // Postgres connection string
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:JPGaDBzwPaKlKTFAWxOdkMazIxopCwoU@postgres.railway.internal:5432/railway',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:ILpVFIbpLIboOkrGeTJQopjppbaoRZac@postgres-3rwb.railway.internal:5432/railway',
 };
-
-
-
-
-
